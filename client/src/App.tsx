b3a711984ab690c9571c74bf7eccf674dad3a97f
Mithila <150042569+NKDMMadhusankha@users.noreply.github.com>
2025-06-09 20:34:53 +0530

    haloooooo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -7,9 +8,26 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
+// Polyfill for useHashLocation
+function useHashLocation() {
+  const hashToPath = () => window.location.hash.replace(/^#/, '') || '/';
+  const [loc, setLoc] = React.useState(hashToPath());
+  React.useEffect(() => {
+    const onHashChange = () => setLoc(hashToPath());
+    window.addEventListener('hashchange', onHashChange);
+    return () => window.removeEventListener('hashchange', onHashChange);
+  }, []);
+  const navigate = (to: string) => {
+    window.location.hash = to;
+    setLoc(hashToPath());
+  };
+  return [loc, navigate] as const;
+}
+
 function Router() {
+  const [location, setLocation] = useHashLocation();
   return (
-    <Switch>
+    <Switch location={location}>
       <Route path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
